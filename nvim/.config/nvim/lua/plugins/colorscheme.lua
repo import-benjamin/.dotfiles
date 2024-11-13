7ff241c3338e047f4d003c97bcae561790545eb0
Benjamin <8467845+import-benjamin@users.noreply.github.com>
2024-11-13 22:46:05 +0100

    feat: use github theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,13 @@
 return {
-  "zenbones-theme/zenbones.nvim",
-  lazy = false,
-  priority = 1000,
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "zenbones",
-    },
-  },
+  "projekt0n/github-nvim-theme",
+  name = "github-theme",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    require("github-theme").setup({
+      -- ...
+    })
+
+    vim.cmd("colorscheme github_dark_high_contrast")
+  end,
 }
