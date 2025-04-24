48be69280a52a1bc70c7ef6278201f629902916d
Benjamin <8467845+import-benjamin@users.noreply.github.com>
2025-04-24 23:46:28 +0200

    refactor: update nvim dependencies and migrate to zenbones

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,10 @@
 return {
-  { "EdenEast/nightfox.nvim" },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "dayfox",
-    },
-  },
+  "zenbones-theme/zenbones.nvim",
+  dependencies = "rktjmp/lush.nvim",
+  lazy = false,
+  priority = 1000,
+  -- you can set set configuration options here
+  config = function()
+    vim.cmd.colorscheme("zenbones")
+  end,
 }
