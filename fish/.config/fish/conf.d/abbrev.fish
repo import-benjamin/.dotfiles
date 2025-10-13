5892f21997cf543d679dd51736b13d04d5dc30c1
Benjamin <8467845+import-benjamin@users.noreply.github.com>
2025-10-13 21:36:01 +0200

    fix: use correct level flag for tree layout with erd

--- fish/.config/fish/conf.d/abbrev.fish
+++ fish/.config/fish/conf.d/abbrev.fish
@@ -1,4 +1,4 @@
 abbr --add glola git log --oneline --graph --all
-abbr --add ll erd --config ls
-abbr --add lt erd --config ls -T 3
+abbr --add ll erd --config ls --level 1
+abbr --add lt erd --config ls --level 3
 abbr --add du erd --config du
